ac3e4f4f589e2393c7e0e8b2011177f819f78d49
SangYoung2 <tmskan8@naver.com>
2023-07-11 21:57:58 +0900

    spring study day3

---
+++ src/main/java/com/example/spring/controller/HelloController.java
@@ -0,0 +1,54 @@
+package com.example.spring.controller;
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // view를 여는 것이 아닌 문자가 그대로 전송된다.
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) { //JSON 방식으로 가져온다.
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // hello의 키는 name이며 값은 setName에 넣은 name값이다.
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
